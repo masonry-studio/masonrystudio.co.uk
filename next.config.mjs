c3c4404fbeed826d97fc760bd113282d8b3a3045
Ty Mason <hi@tygr.dev>
2025-03-08 14:53:02 +0000

    refactor: remove payload from the site

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
@@ -16,4 +15,4 @@ const nextConfig = {
 	},
 };
 
-export default withPayload(nextConfig);
+export default nextConfig;
